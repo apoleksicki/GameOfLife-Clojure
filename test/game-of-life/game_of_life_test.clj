aa8327e74ee046c9fe675617b9153934125d91d9
Antoni Piotr Oleksicki <ap.oleksicki@gmail.com>
2013-03-04 23:14:50 +0100

    Added game_of_life_test.clj.

---
+++ test/game-of-life/game_of_life_test.clj
@@ -0,0 +1,29 @@
+(ns game-of-life.test
+  (:use clojure.test clojure.repl game-of-life.core ))
+
+(def board [[:dead :dead :dead  :dead :dead]
+            [:dead :dead :dead :dead :dead]
+            [:dead :dead :alive :dead :dead]
+            [:dead :dead :alive :dead :dead]
+            [:dead :dead :alive :dead :dead]
+            [:dead :dead :dead :dead :dead]])
+
+(def expected-board 
+  [[:dead :dead :dead :dead :dead]
+   [:dead :dead :dead :dead :dead]
+   [:dead :dead :dead :dead :dead]
+   [:dead :alive :alive :alive :dead]
+   [:dead :dead :dead :dead :dead]
+   [:dead :dead :dead :dead :dead]])
+
+(def vector-string "_ _ _ _ _ \n_ _ _ _ _ \n_ _ * _ _ \n_ _ * _ _ \n_ _ * _ _ \n_ _ _ _ _ ")
+
+
+(defn foo-test []
+  (is (= 4 (+ 2 2)) "Two plus two should be 4"))
+
+(defn calculate-new-board-test []
+  (is (= expected-board (calculate-new-board board))))
+
+(defn string-to-vector-test [] 
+  (is (= vector-string (string-to-vector board))))
