f71482b4caa1a39a246ead6775d2047a3a61c791
Antoni Piotr Oleksicki <Antoni Piotr Oleksicki>
2013-03-05 16:55:49 +0100

    Corrected implementation of string-to-vector-test. Removed variables, that alreardy exist in the core module.

--- test/game-of-life/game_of_life_test.clj
+++ test/game-of-life/game_of_life_test.clj
@@ -1,13 +1,6 @@
 (ns game-of-life.test
   (:use clojure.test clojure.repl game-of-life.core ))
 
-(def board [[:dead :dead :dead  :dead :dead]
-            [:dead :dead :dead :dead :dead]
-            [:dead :dead :alive :dead :dead]
-            [:dead :dead :alive :dead :dead]
-            [:dead :dead :alive :dead :dead]
-            [:dead :dead :dead :dead :dead]])
-
 (def expected-board 
   [[:dead :dead :dead :dead :dead]
    [:dead :dead :dead :dead :dead]
@@ -16,9 +9,6 @@
    [:dead :dead :dead :dead :dead]
    [:dead :dead :dead :dead :dead]])
 
-(def vector-string "_ _ _ _ _ \n_ _ _ _ _ \n_ _ * _ _ \n_ _ * _ _ \n_ _ * _ _ \n_ _ _ _ _ ")
-
-
 (defn foo-test []
   (is (= 4 (+ 2 2)) "Two plus two should be 4"))
 
@@ -26,4 +16,4 @@
   (is (= expected-board (calculate-new-board board))))
 
 (defn string-to-vector-test [] 
-  (is (= vector-string (string-to-vector board))))
+  (is (= board (string-to-vector vector-string))))
